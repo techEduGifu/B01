fd71c4b137d984f71e228391048c5de278c33c2c
D. FUKUOKA <30850626+techEduGifu@users.noreply.github.com>
2020-03-17 18:44:40 +0900

    Updates.

--- main.ts
+++ main.ts
@@ -19,7 +19,7 @@ namespace GUB01 {
     //% weight=80 blockGap=8
     //% parts="GUBoard"
     //% dat.min=0 dat.max=100
-    export function SwitchPWM(dat: number): void {
+    export function SwitchPWM(dat: number): void{
         let val = (dat * 1023) / 100;
         pins.analogWritePin(AnalogPin.P1, val)
     }
