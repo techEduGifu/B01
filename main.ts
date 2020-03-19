e30d8bf0645349d785e2e44e4d0a3a6f430f47b6
D. FUKUOKA <30850626+techEduGifu@users.noreply.github.com>
2020-03-19 12:57:17 +0900

    Updates.

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 //% weight=100 color=#A040E0 icon="\uf0eb" block="拡張ボード（B01）"
 namespace Board01 {
-    //% blockId=B01_SwitchON block="スイッチをオンにする"
+    //% blockId=B01_SwitchON
+    //% block="スイッチをオンにする"
     //% weight=80 blockGap=8
-    //% parts="Board01"
     export function SwitchON(): void {
         
         pins.digitalWritePin(DigitalPin.P1, 1)
@@ -11,8 +11,7 @@ namespace Board01 {
     //% blockId=B01_SwitchPWM
     //% block="スイッチをオンにする（%Value ％）"
     //% weight=80 blockGap=8
-    //% parts="Board01"
-    //% Value.min=0 Value.max=100
+    //% Value.min=0 Value.max=100 Value.defl=100
     export function SwitchPWM(Value: number): void {
         let val = (Value * 1023) / 100;
         pins.analogWritePin(AnalogPin.P1, val)
@@ -21,7 +20,6 @@ namespace Board01 {
     //% blockId=B01_SwitchOFF
     //% block="スイッチをオフにする"
     //% weight=80 blockGap=8
-    //% parts="Board01"
     export function SwitchOFF(): void {
         pins.digitalWritePin(DigitalPin.P1, 0)
     }
@@ -29,7 +27,6 @@ namespace Board01 {
     //% blockId=B01_Sensor
     //% block="人感センサが反応した"
     //% weight=80 blockGap=8
-    //% parts="Board01"
     export function IsDetected(): boolean {
         if (pins.digitalReadPin(DigitalPin.P2) == 1)
             return true;
