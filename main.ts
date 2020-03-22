a2b48e83584e0fe109c75cdc8ff07926c9170606
D. FUKUOKA <30850626+techGifu@users.noreply.github.com>
2020-03-22 20:25:18 +0900

    Updates.

--- main.ts
+++ main.ts
@@ -1,8 +1,7 @@
 //% weight=100 color=#FF9933 icon="\uf0eb" block="拡張ボード（B01）"
 namespace Board01 {
     //% blockId=B01_SwitchON
-    //% block="SwitchON"
-    //% block.loc.ja-jp="スイッチをオンにする"
+    //% block=スイッチをオンにする"
     //% weight=80 blockGap=8
     export function SwitchON(): void {
         
@@ -10,8 +9,7 @@ namespace Board01 {
     }
 
     //% blockId=B01_SwitchPWM
-    //% block="SwitchON (%Value ％)"
-    //% block.loc.jp="スイッチをオンにする（%Value ％）"
+    //% block="スイッチをオンにする（%Value ％）"
     //% weight=80 blockGap=8 advanced=true
     //% Value.min=0 Value.max=100 Value.defl=100
     export function SwitchPWM(Value: number): void {
@@ -20,16 +18,14 @@ namespace Board01 {
     }
 
     //% blockId=B01_SwitchOFF
-    //% block="SwithOFF"
-    //% block.loc.ja_jp="スイッチをオフにする"
+    //% block="スイッチをオフにする"
     //% weight=80 blockGap=8e
     export function SwitchOFF(): void {
         pins.digitalWritePin(DigitalPin.P1, 0)
     }
 
     //% blockId=B01_Sensor
-    //% block="IsDetected"
-    //% block.loc.ja="人感センサが反応した"
+    //% block="人感センサが反応した"
     //% weight=80 blockGap=8
     export function IsDetected(): boolean {
         if (pins.digitalReadPin(DigitalPin.P2) == 1)
