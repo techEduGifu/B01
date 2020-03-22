606d24b81095be9144100258dfb3f7edff38a9dd
D. FUKUOKA <30850626+techGifu@users.noreply.github.com>
2020-03-22 09:54:01 +0900

    Updates.

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-//% weight=100 color=#eecc33 icon="\uf0eb" block="拡張ボード（B01）"
+//% weight=100 color=#663300 icon="\uf0eb" block="拡張ボード（B01）"
 namespace Board01 {
     //% blockId=B01_SwitchON
     //% block="スイッチをオンにする"
